6f9ddbbf57e57fda0b1797809faad632db9faaeb
KonradMarzec1991 <konrimarzec@gmail.com>
2018-11-19 22:41:21 +0100

    CRUD person done

--- people/urls.py
+++ people/urls.py
@@ -1,13 +1,16 @@
-from django.urls import path
-from django.conf.urls import url
-from .views import home, PersonCreateView, PersonListView, \
-                        PersonView
+from django.urls import path, re_path
+from .views import (home,
+                    PersonCreateView,
+                    PersonListView,
+                    PersonView,
+                    PersonUpdateView,
+                    PersonDeleteView)
 
 app_name = 'people'
 urlpatterns = [
     path('person/new', PersonCreateView.as_view(), name="person-add"),
     path('', PersonListView.as_view(), name='person-list'),
-    url(r'^person/show/(?P<id>[0-9]+)/$', PersonView.as_view(), name='person-detail'),
-    # path('<int:id>/update/', CourseUpdateView.as_view(), name='courses-update'),
-    # path('<int:id>/delete/', CourseDeleteView.as_view(), name='courses-delete'),
+    # re_path(r'^person/show/(?P<id>[0-9]+)/$', PersonView.as_view(), name='person-detail'),
+    re_path(r'^person/update/(?P<id>[0-9]+)/$', PersonUpdateView.as_view(), name='person-update'),
+    re_path(r'^person/delete/(?P<id>[0-9]+)/$', PersonDeleteView.as_view(), name='person-delete'),
 ]
