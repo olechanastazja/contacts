8268dbbf88f302dfcd2a78bca3f5be6ce13ac61b
Agnieszka Olech <agnieszka.anstazja.olech@gmail.com>
2018-11-19 20:16:42 +0100

    Update Person

--- people/urls.py
+++ people/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from .views import home, PersonCreateView, PersonListView
-
+from django.conf.urls import url
+from .views import home, PersonCreateView, PersonListView, \
+                        PersonView
 
 app_name = 'people'
 urlpatterns = [
-    # path('', home, name="home_page"),
     path('person/new', PersonCreateView.as_view(), name="person-add"),
     path('', PersonListView.as_view(), name='person-list'),
-    # path('create/', CourseCreateView.as_view(), name='courses-create'),
-    # path('<int:id>/', CourseView.as_view(), name='courses-detail'),
+    url(r'^person/show/(?P<id>[0-9]+)/$', PersonView.as_view(), name='person-detail'),
     # path('<int:id>/update/', CourseUpdateView.as_view(), name='courses-update'),
     # path('<int:id>/delete/', CourseDeleteView.as_view(), name='courses-delete'),
 ]
