799e5996209382d8c517057407cd8e1be64285cc
KonradMarzec1991 <konrimarzec@gmail.com>
2018-11-20 23:52:53 +0100

    The worst part is done!

--- people/views.py
+++ people/views.py
@@ -1,7 +1,8 @@
+from django.forms import modelformset_factory
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .forms import PersonModelForm
-from django.contrib import messages
+from group.models import Group
 from .models import Person
 import json
 from django.http import HttpResponse
@@ -30,15 +31,16 @@ class PersonCreateView(View):
         context = {
             'form': form
         }
-        return render(request, self.template_name,context)
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        form = PersonModelForm(request.user,request.POST)
+        form = PersonModelForm(request.user, request.POST)
         if form.is_valid():
             # person = Person(**form.cleaned_data, user=request.user)
             person = form.save(commit=False)
             person.user = request.user
             person.save()
+            person.group.set(form.cleaned_data['group'])
         return redirect('people:person-list')
 
 
@@ -69,17 +71,21 @@ class PersonUpdateView(PersonObjectMixin, View):
         context = {}
         obj = self.get_object()
         if obj is not None:
-            form = PersonModelForm(instance=obj)
+            form = PersonModelForm(instance=obj, user=request.user)
+
             context['object'] = obj
             context['form'] = form
+
         return render(request, self.template_name, context)
 
     def post(self, request, id=None,  *args, **kwargs):
         context = {}
         obj = self.get_object()
         if obj is not None:
-            form = PersonModelForm(request.POST, instance=obj)
-
+            form = PersonModelForm(instance=obj, user=request.user, data=request.POST)
+            print(obj)
+            print(request.user)
+            print(request.POST)
             if form.is_valid():
                 form.save()
             context['object'] = obj
